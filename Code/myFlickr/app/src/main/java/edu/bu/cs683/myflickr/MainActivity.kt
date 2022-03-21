f2e084e251520a9e43fb1555374d27cc2f8183e2
Dexter Legaspi <1777690+dalegaspi@users.noreply.github.com>
2022-03-20 20:53:52 -0400

    spotless added

--- Code/myFlickr/app/src/main/java/edu/bu/cs683/myflickr/MainActivity.kt
+++ Code/myFlickr/app/src/main/java/edu/bu/cs683/myflickr/MainActivity.kt
@@ -1,9 +1,16 @@
 package edu.bu.cs683.myflickr
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
+/**
+ * @author dlegaspi@bu.edu
+ */
 class MainActivity : AppCompatActivity() {
+
+    val APIKEY = "9f6312e4c2267993ed1555c1e53b7e9a"
+    val APISECRET = "da07bc0f98be24dd"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
--- Code/myFlickr/app/src/test/java/edu/bu/cs683/myflickr/ExampleUnitTest.kt
+++ Code/myFlickr/app/src/test/java/edu/bu/cs683/myflickr/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package edu.bu.cs683.myflickr
 
-import org.junit.Test
-
 import org.junit.Assert.*
+import org.junit.Test
 
 /**
  * Example local unit test, which will execute on the development machine (host).

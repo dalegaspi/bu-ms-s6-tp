ca36ebd51028a1a6206c818a55b5d58bd72def98
Dexter Legaspi <1777690+dalegaspi@users.noreply.github.com>
2022-03-20 20:44:32 -0400

    initial checkin

---
+++ Code/myFlickr/app/src/main/java/edu/bu/cs683/myflickr/MainActivity.kt
@@ -0,0 +1,11 @@
+package edu.bu.cs683.myflickr
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
